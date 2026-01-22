cfe17cfe158389041cf5c0404704ac98319b5b70
Jair Montes <jairmontes12@gmail.com>
2026-01-22 15:05:30 -0500

    Fix login details

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -33,6 +33,7 @@ export class LoginComponent {
       this.router.navigate(['/home']);
     } else {
       console.log('Credenciales inválidas');
+      alert('Credenciales inválidas');
     }
   }
 }
